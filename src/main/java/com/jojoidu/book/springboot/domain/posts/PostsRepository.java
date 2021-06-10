300151d873bdd3a4f53557d4a7bdff7cfdeb32fb
lsw <skek7811@gmail.com>
2021-06-10 16:13:10 +0900

    게시글 리스트 표시, 게시글 수정 추가

--- src/main/java/com/jojoidu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoidu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.jojoidu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    //Jpa에서 기본으로 제공되지 않는 메소드를 쿼리로 작성해도 된다
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
